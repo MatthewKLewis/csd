55d3b9a90203f9dfeab04426300072aaecf8535a
MatthewKLewis <matthew.knight.lewis@gmail.com>
2021-12-14 20:49:38 -0500

    wax museum

--- angular-src/src/app/dialog-frame/add-raw-material/add-raw-material.component.ts
+++ angular-src/src/app/dialog-frame/add-raw-material/add-raw-material.component.ts
@@ -13,6 +13,9 @@ export class AddRawMaterialComponent implements OnInit {
   @Input() public data: any
   addRawMaterialForm: FormGroup;
 
+  disciplines: Array<string> = ['Mining', 'Mushroom Gathering', 'Scrapping', 'Spidersilk Gathering']
+  levels: Array<number> = [0,5,10,15,20,25,30,35,40,45,50,55,60]
+
   constructor(
     public dialogRef: MatDialogRef<DialogFrameComponent>,
     private fb: FormBuilder
@@ -20,7 +23,8 @@ export class AddRawMaterialComponent implements OnInit {
     this.addRawMaterialForm = this.fb.group({
       name: ['', [Validators.required]],
       level: [0, [Validators.required]],
-      description: ['',],
+      description: ['', [Validators.required]],
+      discipline: ['',],
     });
   }
 
